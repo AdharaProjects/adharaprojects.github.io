83f4b6c554d9d05cee282ed43f4da7c7d832093f
Jason Smythe <jason@smythemail.za.net>
2018-07-17 00:05:01 +0300

    Show/hide the logo when the menu is opened/closed (in mobile view)

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,23 @@
-// Offset for Site Navigation
-$('#siteNav').affix({
-	offset: {
-		top: 100
-	}
-})
+$(document).ready(function(){
+	// Offset for Site Navigation
+	$('#siteNav').affix({
+		offset: {
+			top: 100
+		}
+	})
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 50
-    }, 500)
-    return false
-})
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - 50
+		}, 500)
+		return false
+	})
+
+	// Show and hide the icon when the menu is open/closed
+	$('#navbar').on('show.bs.collapse', function () {
+		$("	#header-logo").css("display", "none");
+	})
+	$('#navbar').on('hidden.bs.collapse', function () {
+		$(	"#header-logo").css("display", "block");
+	})
+});
